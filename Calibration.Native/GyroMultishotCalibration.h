becf9284e419c28cec4639265ae9f0da9ad03422
Luiso <lecc628@gmail.com>
2022-05-27 07:40:44 +0200

    Add the GyroMultishotCalibrationResult struct

--- Calibration.Native/GyroMultishotCalibration.h
+++ Calibration.Native/GyroMultishotCalibration.h
@@ -46,4 +46,38 @@ typedef struct GyroMultishotCalibrationArgs
 	GyroMultishotConfiguration Data[GYRO_MULTISHOT_CONFIGURATION_DATA_COUNT];
 } GyroMultishotCalibrationArgs;
 
+typedef struct GyroMultishotConfigurationError
+{
+	double Azimuth;
+	double Inclination;
+	double Toolface;
+
+	double AxError;
+	double AyError;
+	double AzError;
+} GyroMultishotConfigurationError;
+
+typedef struct GyroMultishotCalibrationResult
+{
+	double KwX;
+	double DeltaX;
+	double RoX;
+	double EpsX;
+	double Eta1X;
+	double Eta2X;
+	double TauX;
+	double AlpwXY;
+
+	double KwY;
+	double DeltaY;
+	double RoY;
+	double EpsY;
+	double Eta1Y;
+	double Eta2Y;
+	double TauY;
+	double AlpwYX;
+
+	GyroMultishotConfigurationError Errors[GYRO_MULTISHOT_CONFIGURATION_DATA_COUNT];
+} GyroMultishotCalibrationResult;
+
 _COMMON_END_EXTERN_C_
