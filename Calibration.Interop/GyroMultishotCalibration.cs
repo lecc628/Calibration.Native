1bb9ed5e5fdb5a2f52182e67f232ebcaae467195
Luiso <lecc628@gmail.com>
2022-05-28 13:54:09 +0200

    Add interop for GyroMultishotCalibrationResult

--- Calibration.Interop/GyroMultishotCalibration.cs
+++ Calibration.Interop/GyroMultishotCalibration.cs
@@ -39,4 +39,41 @@ namespace Calibration.Interop
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.GyroMultishotConfigurationDataCount)]
         public GyroMultishotConfiguration[] Data;
     }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GyroMultishotConfigurationError
+    {
+        public double Azimuth;
+        public double Inclination;
+        public double Toolface;
+
+        public double AxError;
+        public double AyError;
+        public double AzError;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GyroMultishotCalibrationResult
+    {
+        public double KwX;
+        public double DeltaX;
+        public double RoX;
+        public double EpsX;
+        public double Eta1X;
+        public double Eta2X;
+        public double TauX;
+        public double AlpwXY;
+
+        public double KwY;
+        public double DeltaY;
+        public double RoY;
+        public double EpsY;
+        public double Eta1Y;
+        public double Eta2Y;
+        public double TauY;
+        public double AlpwYX;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.GyroMultishotConfigurationDataCount)]
+        public GyroMultishotConfigurationError[] Errors;
+    }
 }
